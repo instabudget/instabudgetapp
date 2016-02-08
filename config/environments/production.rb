36b5cfaee29e9a54499834970fa4a1f9bb1c6366
MachbooSS <tulefat@gmail.com>
2016-02-08 14:40:10 -0500

    Updated Brand and Mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sampleappraed.herokuapp.com'
+  host = 'instabudget.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -77,10 +77,6 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  # $ heroku config:get SENDGRID_USERNAME
-  # app47033570@heroku.com
-  # $ heroku config:get SENDGRID_PASSWORD
-  # 3ombijb40671
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
